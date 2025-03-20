f435d97c6e939b78052d0cd85a4eb457a2d91efd
Guster <gusterwoei@gmail.com>
2025-03-20 22:40:16 +0800

    generate remote app - about

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'about',
+    loadChildren: () => import('about/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'shop',
     loadChildren: () => import('shop/Routes').then((m) => m!.remoteRoutes),
   },
